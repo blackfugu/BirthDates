ff1bb62745d61b6b27af12718bf7dd9818f85087
Aaron Puffer <blackfugu@users.noreply.github.com>
2020-06-06 20:15:03 -0400

    Add Project to Source

---
+++ FirstResponsiveWebAppLastName/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstResponsiveWebAppLastName.Models;
+
+namespace FirstResponsiveWebAppLastName.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            PersonModel person = new PersonModel();
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult Index(PersonModel person)
+        {
+            return View(person);
+        }
+
+        public ActionResult CalculateByDate()
+        {
+            PersonExactDateModel person = new PersonExactDateModel();
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult CalculateByDate(PersonExactDateModel personModel)
+        {
+            return View(personModel);
+        }
+
+    }
+}
---
+++ FirstResponsiveWebAppLastName/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstResponsiveWebAppLastName.MvcApplication" Language="C#" %>
